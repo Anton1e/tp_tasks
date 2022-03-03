a039dd0a9abc0fe500f0f0e1adfade38653f73aa
Anton1e <le.ad@phystech.edu>
2022-03-03 19:05:59 +0300

    find used in backup function and documentation added

--- task1/run.sh
+++ task1/run.sh
@@ -1,51 +1,36 @@
 #!/bin/bash 
 
-#counter of backed up files
-couter=0
+# DOCUMENTATION
+# script takes in 8 arguments:
+#--input_folder INPUT_FOLDER --extension EXTENSION --backup_folder BACKUP_FOLDER --backup_archive_name BACKUP_ARCHIVE_NAME in random order
+# it takes all files in INPUT_FOLDER and its directories with 
+# EXTENSION and copies them all to BACKUP_FOLDER. Then an archive with 
+# BACKUP_ARCHIVE_NAME is made from the BACKUP_FOLDER
 
-# backup funtion (input_folder, extension, backup folder, backup archive)
-# tranverse through directory recursively
-# if file is a directory go into it
-# else check if it has the needed extension
-# if yes copy it otherwise pass
+# counter of backed up files
+counter=0
+
+# backup (input_folder, extension, backup_folder)
+# use find to get all files with needed extension in 
+# input_folder and its directories
+# copy them to backup_folder and change thier names into
+# {counter}_FILE_NAME
 backup () {
-    for file in $1/*
+    for file in $(find $1 -name "*.$2")
     do
-        if [ -d $file ];
-        then
-            backup $file $2 $3
-        else
-            if [ ${file##*.} == $2 ];
-            then
-                file_name=$(basename -- $file)
-                cp $file ./$3
-                #rename file_name into 
-                #FILE_NAME_{number of backed up files}.FILE_EXTENSION
-                new_file_name="${file_name#*.}_$(( ++counter )).${file_name##*/}" 
-                mv ./$3/$file_name ./$3/$new_file_name
-            fi
-        fi          
+        cp $file ./$3
+        mv ./$3/$(basename $file) ./$3/"$(( ++counter ))_$(basename $file)"
     done
 }
 
 
 # reading input
-input=($1 $2 $3 $4 $5 $6 $7 $8)
-for i in 0 2 4 6
-do
-    if [ ${input[i]} == "--input_folder" ];
-    then 
-        input_folder=${input[$(( i + 1 ))]}
-    elif [ ${input[i]} == "--extension" ];
-    then
-        extension=${input[$(( i + 1 ))]}
-    elif [ ${input[i]} == "--backup_folder" ];
-    then 
-        backup_folder=${input[$(( i + 1 ))]}
-    else 
-        backup_archive_name=${input[$(( i + 1 ))]}
-    fi
-done
+# declaring dict with input information
+declare -A input=( ["$1"]="$2" ["$3"]="$4" ["$5"]="$6" ["$7"]="$8" )
+input_folder=${input["--input_folder"]}
+extension=${input["--extension"]}
+backup_folder=${input["--backup_folder"]}
+backup_archive_name=${input["--backup_archive_name"]}
 
 #backup to backup_folder and make an archive from it
 mkdir ./$backup_folder
